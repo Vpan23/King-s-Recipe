ecf4968a2c8dd505990c5b59c91bb1a3f25ad05e
Code Heart <119617297+Vpan23@users.noreply.github.com>
2023-12-28 17:53:13 -0500

    nuevos arreglos y finalizacion mobile.

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const btnNavbar = document.querySelector('.btn__menu');
-btnNavbar.addEventListener('click', (e) => {
-    console.log('funciona')
-})
+const btnNavbar = document.querySelector(".btn__menu");
+const hiddenContent = document.querySelector(".hidden-content");
+
+let activador = true;
+btnNavbar.addEventListener("click", (e) => {
+  console.log("funciona");
+  if (activador) {
+    hiddenContent.style.width = `${0}px`;
+    activador = false;
+  } else {
+    hiddenContent.style.width = `${320}px`;
+    activador = true;
+  }
+});
+document.querySelector('a[href^="#"]').addEventListener('click', function (e) {
+    e.preventDefault();
+  
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+  
